e2cf6baac04898ec53546efbd051fb05739fffda
neethini <118648821+neethini@users.noreply.github.com>
2023-06-06 14:59:19 +0530

    Add files via upload

---
+++ Neethini/FrontEnd/src/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Main() {
+
+    return (
+        <div className=' justify-content-center align-items-center bg-primary vh-100'>
+            <h1>My First App</h1>
+            <br />
+            <p>Welcome Loading.......</p>
+        </div>
+    )
+
+}
+export default Main;
