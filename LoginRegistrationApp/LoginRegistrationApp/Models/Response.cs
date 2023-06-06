73de8da5099d3d974eef2c5377c7dad16fe17486
neethini <118648821+neethini@users.noreply.github.com>
2023-06-06 14:51:39 +0530

    Delete LoginRegistrationApp directory

--- LoginRegistrationApp/LoginRegistrationApp/Models/Response.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LoginRegistrationApp.Models
-{
-    public class Response
-    {
-        public int statusCode { get; set; }
-        public string? statusMessage { get; set; }
-    }
-}
